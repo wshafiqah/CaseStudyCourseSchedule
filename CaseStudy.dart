6d3749299bc3f556513f553443d8c49cfbcf2bd6
afiqahhassan <61734893+afiqahsssssssss@users.noreply.github.com>
2022-05-26 20:49:34 +0800

    Update and rename question2.dart to CaseStudy.dart

--- CaseStudy.dart
+++ CaseStudy.dart
@@ -1,25 +1,7 @@
 import 'dart:io';
+import 'CS2.dart';
 
-class Subject {
-  late int id;
-  late String courseCode;
 
-  Subject(int id, String courseCode) {
-    this.id = id;
-    this.courseCode = courseCode;
-    
-  }
-}
-class Schedule {
-  late int id;
-  late String courseCode;
-  List<Subject> items = [];
-  Schedule(int id, String courseCode) {
-    this.id = id;
-    this.courseCode = courseCode;
-  
-  }
-}
 void main() {
   var currentItemId = 1;
   //var currentStorageId = 1;
